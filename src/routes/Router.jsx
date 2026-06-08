ab50243d0a9e8d7f8c98d2f8fa94f3d783546576
djabranemmd <mammadi.a.djabrane@gmail.com>
2026-06-08 18:59:56 +0100

    fix: add comparison page route

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,22 +1,41 @@
-import { Routes, Route } from "react-router-dom";
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import MainLayout from "../layouts/MainLayout";
 
 import HomePage from "../pages/HomePage";
 import AlgorithmPage from "../pages/AlgorithmPage";
+import ComparisonPage from "../pages/ComparisonPage";
 
 function Router() {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
+      <Route
+        element={
+          <MainLayout />
+        }
+      >
         <Route
           path="/"
-          element={<HomePage />}
+          element={
+            <HomePage />
+          }
         />
 
         <Route
           path="/algorithm/:slug"
-          element={<AlgorithmPage />}
+          element={
+            <AlgorithmPage />
+          }
+        />
+
+        <Route
+          path="/comparison"
+          element={
+            <ComparisonPage />
+          }
         />
       </Route>
     </Routes>
